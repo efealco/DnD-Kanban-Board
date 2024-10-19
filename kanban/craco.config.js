30cacd4f53be644d1cb961819a01dd77c8d17cd1
Efe Alço <efe.alco@cloudflight.io>
2024-10-20 01:10:48 +0300

    initial commit

---
+++ kanban/craco.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    style: {
+        postcss: {
+            plugins: [
+                require('autoprefixer'),
+                // Add more PostCSS plugins if needed
+            ],
+        },
+    },
+};
